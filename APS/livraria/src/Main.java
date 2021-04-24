e77cbd555f66d2e41b666624a49a97310aaf97ab
Lu <maximoffxstan@gmail.com>
2021-04-24 17:01:06 -0300

    Modificação do main

--- APS/livraria/src/Main.java
+++ APS/livraria/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+            Livro l1 = new Livro("Harry Potter", "JK Rolling", "Record", 390);
+            l1.info();
+            l1.abrir();
+            l1.irPag(105);
+            l1.fechar();
+            l1.info();
     }
 }
