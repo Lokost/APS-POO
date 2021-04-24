492156f0b30d097d473a47e6e3d35fd6f3ee7cb5
Lokost <51063660+Lokost@users.noreply.github.com>
2021-04-24 15:18:46 -0300

    AT Classe Livro

--- APS/livraria/src/Livro.java
+++ APS/livraria/src/Livro.java
@@ -1,11 +1,58 @@
 public class Livro {
     protected String nome, autor, editora;
-    protected int paginas;
+    protected int paginas, atPag;
+    protected boolean aberto;
 
     public Livro(String nome, String autor, String editora, int paginas) {
         this.nome = nome;
         this.autor = autor;
         this.editora = editora;
         this.paginas = paginas;
+        this.aberto = false;
+        this.atPag = 1;
+    }
+
+    public void abrir() {
+        if (!aberto) {
+            this.aberto = true;
+            System.out.println("---- * ----");
+            System.out.println("O livro foi aberto.");
+        } else {
+            System.out.println("---- * ----");
+            System.out.println("O livro já está aberto.");
+        }
+    }
+
+    public void fechar() {
+        if (aberto) {
+            this.aberto = false;
+            System.out.println("---- * ----");
+            System.out.println("O livro foi fechado.");
+        } else {
+            System.out.println("---- * ----");
+            System.out.println("O livro já está fechado.");
+        }
+    }
+
+    public void info() {
+        System.out.println("---- * ----");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Editora: " + this.editora);
+        System.out.println("Nº de páginas: " + this.paginas);
+        System.out.println("Aberto: " + this.aberto);
+    }
+
+    public void irPag(int pag) {
+        if (pag < 1) {
+            System.out.println("---- * ----");
+            System.out.println("Não é possível ir para uma página abaixo de 1");
+        } else if(pag > this.paginas) {
+            System.out.println("---- * ----");
+            System.out.println("O livro não tem essa página");
+        } else {
+            System.out.println("---- * ----");
+            System.out.printf("Avançado da página: %d%n para a página: %d%n" , this.atPag, pag);
+        }
     }
 }
