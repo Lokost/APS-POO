3b2954f5643807b3ed964de28a8a1c78292bd57d
AtirarLouco <lucasd.araujo@hotmail.com>
2021-04-25 02:24:46 -0300

    adição interface

---
+++ APS/src/livraria/Interface.java
@@ -0,0 +1,16 @@
+package livraria;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public interface Interface{     
+        void dados();
+    
+}
